67ad29507b0490a126c0845d61c94d1431697e9a
Shelby Suits <shelby.suits@woven-planet.global>
2023-04-28 12:50:16 -0500

    initial commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.sample_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:sample_app/app/app.dart';
+import 'package:sample_app/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
